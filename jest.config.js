f6954e4f59c7a387691dc32efbe68e07eaca2614
杜娟 <jdu.xian@gmail.com>
2020-06-19 16:02:15 +0800

    add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  transform: {
+    "^.+\\.(js)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|scss|sass)$": "identity-obj-proxy",
+  },
+};
---
+++ src/__test__/app.test.js
@@ -0,0 +1,3 @@
+test("add 1+1 is 2", () => {
+  expect(2).toBe(2);
+});
